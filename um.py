95f6a78a2209e35d66ed57814b27b7721d6b2ee6
desenv-1dl <desenv.1dl@gmail.com>
2016-10-17 09:19:55 -0200

    Delete um.py

--- um.py
+++
@@ -1,5 +0,0 @@
-self.canvas.setMapTool(self.toolEmit[indToolsShape][indTools])
-
- @pyqtSlot(QgsGeometry, str)
-def on_cadTool(self, geom, command):
-    CF.createFeature(self.canvas, geom, command)
